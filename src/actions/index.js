c13f0fc455d89d582a4e45c2ae444e844b789fcb
Ashley <AshleyReneeLowry@gmail.com>
2018-04-24 16:09:17 -0500

    flesh out searchbar. Add input change handler and stub form submit

---
+++ src/actions/index.js
@@ -0,0 +1,2 @@
+// Key for https://openweathermap.org/forecast5
+const API_KEY = '54e103461aacea8cdd1d3a5102eb4073';
--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -1,11 +1,35 @@
 import React, { Component } from 'react';
 
 export default class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { term: '' };
+
+    // Since we didn't use the fat arrow function that keeps context,
+    // we need to bind onInput change to "this"
+    this.onInputChange = this.onInputChange.bind(this);
+  }
+
+  onInputChange(event) {
+    this.setState({ term: event.target.value });
+  }
+
+  onFormSubmit(event) {
+    event.preventDefault(); // Stop the browser submitting the form- causes refresh.
+  }
+
   render() {
     return (
-      <form className="input-group">
-        <input />
-        <span class="input-group-btn">
+      <form
+        onSubmit={ this.onFormSubmit }
+        className="input-group">
+        <input
+          className="form-control"
+          placeholder="Get a five-day forecast in your favorite cities."
+          onChange={ this.onInputChange }
+          value= { this.state.term } />
+        <span className="input-group-btn">
           <button type="submit" className="btn btn-secondary">
             Submit
           </button>
